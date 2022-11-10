8dc4a8b51358981cee060a6e0d4b4e9ba67bf1c3
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-11-10 19:19:01 +0700

    setup base

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Sucessfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on port: ${process.env.PORT}`);
+});
